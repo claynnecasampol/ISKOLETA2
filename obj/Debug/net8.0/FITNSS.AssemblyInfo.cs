5c4f08b2e1408f3397a511c6ca6ff32f272ec315
Claynne <casampolclaynne@gmail.com>
2025-09-15 23:07:28 +0800

    fix the styling of the active tabs and acad registration submission before and after

--- obj/Debug/net8.0/FITNSS.AssemblyInfo.cs
+++ obj/Debug/net8.0/FITNSS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FITNSS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab5942de74306e52aadeac0046a6769bb50c90a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee90d3da7cf6aaa9936f6e7bec1f959855eee0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FITNSS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FITNSS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
